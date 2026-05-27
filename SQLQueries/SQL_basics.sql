71e61723a2357afcd8cb6d600187f494e2455168
pranet-999 <praneethsharma999@gmail.com>
2026-05-27 20:02:15 +0530

    Update SQL_basics.sql

--- SQLQueries/SQL_basics.sql
+++ SQLQueries/SQL_basics.sql
@@ -78,49 +78,79 @@ From orders
 Select MIN(price)
 From orders
 
+--Just first 2 rows
+--limit
+Select * 
+from orders
+LIMIT 3
+
+--last 2 rows
+Select * 
+from orders
+OFFSET 3
+
+-- no of rows -100
+-- I want first 20 rows--->limit 20
+-- I want last 80 rows--->offset 20
+-- I want last 20 rows---offset 80
+-- I want  rows from 20 t0 40---limit 21 offset 19
+-- I want orders order id 3--->limit 1 offset 2
+-- Skip first 2 orders then show next 2 orders--->
+
+
+Select *
+from orders
+limit 1
+offset 3
+
+--100  -80rows 40 rows
+
+-- I want to know from which cities my customers belongs to
+
+
+--Distinct
+Select DISTINCT city
+from orders
+
+--IN opererator
+
+Select * 
+from orders
+where city IN ('Hyderabad','Vizag')
+
+--Between
+--find orders where price is between 30 and 300
+Select * 
+from orders
+Where price between 30 and 300
+
+--LIKE
+--Supoose I want to get customer name starting with R
+Select * 
+from orders
+Where customer_name LIKE 'R%'
+
+Select * 
+from orders
+Where customer_name NOT LIKE '%n'
+
+Select * 
+from orders
+Where customer_name LIKE '%i%'
+
+--NOT IN
+Select * 
+from orders
+where city NOT IN ('Hyderabad','Vizag')
 
 
 
+Select Max(price)
+From orders
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Select product_name,price
+from orders
+Where price=(Select Max(price)
+From orders)
